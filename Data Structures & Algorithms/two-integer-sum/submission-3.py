2aae3642130e3e8f65629e7335c77c31e1d8301c
Nitai7846 <62793438+Nitai7846@users.noreply.github.com>
2026-09-24 12:44:02 -0500

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.py
@@ -0,0 +1,22 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        n = len(nums)
+        freq = {}
+
+        for i in range(len(nums)):
+            
+            diff = target - nums[i]
+            if diff in freq:
+                return [freq[diff], i]
+            
+            freq[nums[i]] = i 
+        
+   
+
+         
+
+        
+
+
+        
